0f9814db1627a5ec0b4e7bc1bae46b3320790666
kohbanye <kohbanye@gmail.com>
2020-10-24 01:04:29 +0900

    add introduction

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <template>
-  <h1>Hello.</h1>
+  <div :class="$style.container">
+    <h1 :class="$style.hello">Hello.</h1>
+    <card :class="$style.card" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Card from './components/Card.vue'
 
 export default defineComponent({
   name: 'App',
+  components: {
+    Card
+  },
   setup() {
     return {}
   }
 })
 </script>
 
-<style></style>
+<style lang="scss" module>
+@import url('https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c');
+
+html {
+  background-color: #fef8ec;
+}
+.container {
+  padding: 10rem 0;
+}
+.hello {
+  color: #0095a8;
+  text-align: center;
+  font-family: 'M PLUS Rounded 1c', sans-serif;
+  font-size: 4rem;
+  margin: 0;
+  margin-bottom: 10rem;
+}
+.card {
+  width: 50%;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,51 @@
+<template>
+  <div :class="$style.container">
+    <div :class="$style.me">
+      <img src="../assets/monar.png" :class="$style.icon" />
+      <p :class="$style.name">Koh Sakano</p>
+    </div>
+    <p :class="$style.introduce">
+      こんにちは。楽しく生きてます。
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'Card',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" module>
+@import url('https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c');
+
+.container {
+  background-color: #0095a8;
+  text-align: center;
+  border-radius: 0.5rem;
+  padding: 1rem;
+}
+.me {
+  display: flex;
+  justify-content: left;
+  margin-left: 1rem;
+  .icon {
+    height: 4rem;
+    border-radius: 2rem;
+  }
+  .name {
+    color: #ffffff;
+    font-family: 'M PLUS Rounded 1c', sans-serif;
+    font-size: 1.5rem;
+    margin: auto 0 auto 1rem;
+  }
+}
+.introduce {
+  color: #ffffff;
+  font-family: 'M PLUS Rounded 1c', sans-serif;
+}
+</style>
