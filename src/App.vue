58dc3d0c2354eb0e60e0fbb12ddfcf1d3f1f756e
kohbanye <kohbanye@gmail.com>
2020-10-23 22:27:43 +0900

    base

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hello.</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style></style>
